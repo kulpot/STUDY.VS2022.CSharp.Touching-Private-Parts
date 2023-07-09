5e1143d5d13540f064f0376a59e5fd2feff0c2e7
Kulpot <kulpotwahu@gmail.com>
2023-07-10 03:47:09 +0800

    Add project files.

---
+++ Touching Private Parts/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+
+//ref link:https://www.youtube.com/watch?v=VPlyPfM8hMQ&list=PLRwVmtr-pp05brRDYXh-OTAIi-9kYcw3r&index=25
+//  google search - code access security - 
+
+class Cow
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    private int NumHeartBeats { get; set; }
+    public void Beat() { NumHeartBeats++; }
+    private void Digest() { Console.WriteLine("grind grind..."); }
+}
+
+class MainClass
+{
+    static void Main()
+    {
+        Cow betsy = new Cow { Name = "Betsy", Age = 5 };
+        betsy.Beat(); betsy.Beat(); betsy.Beat(); betsy.Beat(); betsy.Beat();
+        //Console.WriteLine(betsy.numHeartBeats);
+        PropertyInfo propInfo = typeof(Cow).GetProperty("NumHeartBeats", 
+            BindingFlags.NonPublic | BindingFlags.Instance);
+        int numBeats = (int)propInfo.GetValue(betsy, null);
+        Console.WriteLine(numBeats);
+    }
+}
